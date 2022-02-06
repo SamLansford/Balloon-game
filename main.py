4264b54d9bccbd2ceeb0ff8a1afc676179433821
Sam Lansford <75640963+SamLansford@users.noreply.github.com>
2022-02-06 05:23:43 +0000

    Feat: creating a pop up screen

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ import pygame
 
 pygame.init()
 
-WIDTH = 450
-HEIGHT = 400
+WIDTH = 400
+HEIGHT = 450
 
-widow = pygame.display.set_mode((WIDTH, HEIGHT))
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ballon Game")
+
+FPS = 60
+
+clock = pygame.time.Clock()
+
+Start = True
+
+while Start:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+window.fill(255, 0 ,0)
+
+pygame.display.update()
+
+clock.tick(FPS)
